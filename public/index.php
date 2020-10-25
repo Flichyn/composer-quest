c15bd54822f431d91c0cbc73646b42046d6e634b
Flichyn <nicolasflichy@gmail.com>
2020-10-25 10:03:37 +0100

    Made changes

--- public/index.php
+++ public/index.php
@@ -10,4 +10,6 @@ $twig = new Twig\Environment($loader, [
 
 $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
 
-echo $twig->render('index.html.twig', $products);
+echo $twig->render('index.html.twig', [
+    'products' => $products,
+]);
